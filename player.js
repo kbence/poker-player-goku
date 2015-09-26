12829e0b627cbf971aa8015898a7b6e5c69766f5
Bence Kiglics <bence.kiglics@gmail.com>
2015-09-26 12:06:02 +0200

    Bluff

--- player.js
+++ player.js
@@ -8,11 +8,17 @@ module.exports = {
     var cards = player.hole_cards
 
     if ('AK'.indexOf(cards[0].rank) != -1 && 'AK'.indexOf(cards[1].rank) != -1) {
-      return 10000;
+      return player.stack;
     }
 
     if (cards[0].rank == cards[1].rank) {
       return (int)(player.stack / 2);
+    }
+
+    if ((player.in_action + 2) % 4 == player.dealer &&
+        game_state.pot < 8 * game_state.small_blind)
+    {
+      return player.stack;
     }
 
     return 0;
