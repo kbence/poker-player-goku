f92884869c6d868ab4fb6e488be4fe8c6339678c
Bence Kiglics <bence.kiglics@gmail.com>
2015-09-26 16:55:50 +0200

    more clever test

--- test.sh
+++ test.sh
@@ -1,48 +1,14 @@
+#!/bin/bash
+
 PORT=1234 node player_service.js &
 PID=$!
 
 trap "kill -TERM $PID" EXIT
 
-sleep 2
+sleep 1
 
-curl -d 'action=bet_request&game_state={
-  "players":[
-    {
-      "name":"Player 1",
-      "stack":1000,
-      "status":"active",
-      "bet":0,
-      "hole_cards":[
-        {
-          "rank": "7",
-          "suit": "diamonds"
-        }, {
-          "rank": "K",
-          "suit": "diamonds"
-        }
-      ],
-      "version":"Version name 1",
-      "id":0
-    },
-    {
-      "name":"Player 2",
-      "stack":1000,
-      "status":"active",
-      "bet":0,
-      "hole_cards":[],
-      "version":"Version name 2",
-      "id":1
-    }
-  ],
-  "tournament_id":"550d1d68cd7bd10003000003",
-  "game_id":"550da1cb2d909006e90004b1",
-  "round":0,
-  "bet_index":0,
-  "small_blind":10,
-  "orbits":0,
-  "dealer":0,
-  "community_cards":[],
-  "current_buy_in":0,
-  "in_action": 0,
-  "pot":0
-}' http://localhost:1234/
+for i in $(seq 1 100); do
+  json="$(node test_player.js)"
+  curl -s -d 'action=bet_request&game_state='"$json" http://localhost:1234/
+  echo -n ' ';
+done
---
+++ test_player.js
@@ -0,0 +1,72 @@
+var ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+var suits = ['diamonds', 'hearts', 'clubs', 'spades'];
+
+function pick(list) {
+  return list[Math.floor(Math.random() * list.length)];
+}
+
+function randomCard() {
+  return {
+    rank: pick(ranks),
+    suit: pick(suits)
+  }
+}
+
+function randomCommunityCards() {
+  var commCards = [];
+  var rnd = Math.random();
+
+  if (rnd > 0.5) {
+    commCards.push(randomCard());
+    commCards.push(randomCard());
+    commCards.push(randomCard());
+  }
+
+  if (rnd > 0.6) {
+    commCards.push(randomCard());
+  }
+
+  if (rnd > 0.8) {
+    commCards.push(randomCard());
+  }
+
+  return commCards;
+}
+
+function generateGameState() {
+  return {
+    "players":[
+      {
+        "name":"Player 1",
+        "stack":Math.floor(Math.random() * 1000),
+        "status":"active",
+        "bet":0,
+        "hole_cards":[randomCard(), randomCard()],
+        "version":"Version name 1",
+        "id":0
+      },
+      {
+        "name":"Player 2",
+        "stack":1000,
+        "status":"active",
+        "bet":0,
+        "hole_cards":[],
+        "version":"Version name 2",
+        "id":1
+      }
+    ],
+    "tournament_id":"550d1d68cd7bd10003000003",
+    "game_id":"550da1cb2d909006e90004b1",
+    "round":0,
+    "bet_index":0,
+    "small_blind":10,
+    "orbits":0,
+    "dealer":0,
+    "community_cards": randomCommunityCards(),
+    "current_buy_in":50,
+    "in_action": 0,
+    "pot":70
+  }
+}
+
+console.log(JSON.stringify(generateGameState()));
