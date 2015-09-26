d1a322f1a013caedbf4481f206ddccf96b4420f5
Bence Kiglics <bence.kiglics@gmail.com>
2015-09-26 11:52:38 +0200

    Changed to JS

---
+++ player.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+
+  VERSION: "Default JavaScript folding player",
+
+  bet_request: function(game_state) {
+    return 0;
+  },
+
+  showdown: function(game_state) {
+
+  }
+};
---
+++ player_service.js
@@ -0,0 +1,29 @@
+var player = require('./player');
+var express = require('express');
+var app = express();
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get('/', function(req, res){
+  res.send(200, 'OK')
+});
+
+app.post('/', function(req, res){
+  if(req.body.action == 'bet_request') {
+    res.send(200, player.bet_request(JSON.parse(req.body.game_state)).toString());
+  } else if(req.body.action == 'showdown') {
+    player.showdown(JSON.parse(req.body.game_state));
+    res.send(200, 'OK');
+  } else if(req.body.action == 'version') {
+    res.send(200, player.VERSION);
+  } else {
+    res.send(200, 'OK')
+  }
+
+});
+
+port = parseInt(process.env['PORT'] || 1337);
+host = "0.0.0.0";
+app.listen(port, host);
+console.log('Listening at http://' + host + ':' + port)
