dd6b2bca7f9dc17303aed195966724f1817cd0fc
Bence Kiglics <bence.kiglics@gmail.com>
2015-09-26 13:18:53 +0200

    Added same suit strategy

--- player.js
+++ player.js
@@ -1,24 +1,36 @@
 
+function cardValue(card) {
+  return 2 + ('234567891JQKA'.indexOf(card.rank.charAt(0)));
+}
+
 module.exports = {
 
-  VERSION: "Goku JS - it's over 9001",
+  VERSION: "Goku JS - it's over 9002",
 
   bet_request: function(game_state) {
     var player = game_state.players[game_state.in_action];
     var cards = player.hole_cards
 
+    var all_in = player.stack;
+
     if ('AK'.indexOf(cards[0].rank) != -1 && 'AK'.indexOf(cards[1].rank) != -1) {
-      return player.stack;
+      return all_in;
     }
 
     if (cards[0].rank == cards[1].rank) {
-      return Math.floor(player.stack / 2);
+      return all_in;
+    }
+
+    if (cards[0].suit == cards[1].suit &&
+        Math.abs(cardValue(cards[0]) - cardValue(cards[1])) == 1)
+    {
+      return all_in;
     }
 
     if ((player.in_action + 2) % 4 == player.dealer &&
         game_state.pot < 8 * game_state.small_blind)
     {
-      return player.stack;
+      return all_in;
     }
 
     return 0;
